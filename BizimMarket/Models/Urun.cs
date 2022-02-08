7b1ec66f9b49e12cc236e80cbcdca89a1e943bd4
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2022-02-08 12:50:59 +0300

    Add project files.

---
+++ BizimMarket/Models/Urun.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BizimMarket.Models
+{
+    public class Urun
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Ad { get; set; }
+
+        public decimal Fiyat { get; set; }
+
+        public string ResimYolu { get; set; }
+
+
+
+        public int KategoriId { get; set; }
+        public Kategori Kategori { get; set; }
+    }
+}
