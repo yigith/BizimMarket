ae23f589611540dd1ae1ce6cdad0cff0359c5681
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2022-02-11 12:56:20 +0300

    ürün düzenleme

--- BizimMarket/Areas/Admin/Controllers/UrunlerController.cs
+++ BizimMarket/Areas/Admin/Controllers/UrunlerController.cs
@@ -1,6 +1,7 @@
 ﻿using BizimMarket.Areas.Admin.Models;
 using BizimMarket.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -31,9 +32,7 @@ namespace BizimMarket.Areas.Admin.Controllers
 
         public IActionResult Yeni()
         {
-            ViewBag.Kategoriler = _db.Kategoriler
-                .Select(x => new SelectListItem(x.Ad, x.Id.ToString()))
-                .ToList();
+            KategorileriYukle();
             return View();
         }
 
@@ -42,35 +41,105 @@ namespace BizimMarket.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-                #region Resim Kaydetme
-                string dosyaAdi = null;
-                if (vm.Resim != null)
-                {
-                    dosyaAdi = Guid.NewGuid() + Path.GetExtension(vm.Resim.FileName);
-                    string kaydetmeYolu = Path.Combine(_env.WebRootPath, "img", "urunler", dosyaAdi);
-                    using (var fs = new FileStream(kaydetmeYolu, FileMode.Create))
-                    {
-                        vm.Resim.CopyTo(fs);
-                    } 
-                }
-                #endregion
-
                 Urun urun = new Urun()
                 {
                     Ad = vm.Ad,
                     Fiyat = vm.Fiyat.Value,
                     KategoriId = vm.KategoriId.Value,
-                    ResimYolu = dosyaAdi
+                    ResimYolu = ResimYukle(vm.Resim)
                 };
                 _db.Add(urun);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
+            KategorileriYukle();
+            return View();
+        }
+
+        private string ResimYukle(IFormFile file)
+        {
+            string dosyaAdi = null;
+            if (file != null)
+            {
+                dosyaAdi = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                string kaydetmeYolu = Path.Combine(_env.WebRootPath, "img", "urunler", dosyaAdi);
+                using (var fs = new FileStream(kaydetmeYolu, FileMode.Create))
+                {
+                    file.CopyTo(fs);
+                }
+            }
+
+            return dosyaAdi;
+        }
+
+        public IActionResult Sil(int id)
+        {
+            var urun = _db.Urunler.Find(id);
+            if (urun == null) return NotFound();
+            ResimSil(urun.ResimYolu);
+            _db.Urunler.Remove(urun);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Duzenle(int id)
+        {
+            var urun = _db.Urunler.Find(id);
+            if (urun == null) return NotFound();
+
+            var vm = new UrunDuzenleViewModel()
+            {
+                Id = urun.Id,
+                Ad = urun.Ad,
+                Fiyat = urun.Fiyat,
+                KategoriId = urun.KategoriId,
+                ResimYolu = urun.ResimYolu
+            };
+
+            KategorileriYukle();
+            return View(vm);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Duzenle(UrunDuzenleViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var urun = _db.Urunler.Find(vm.Id);
+                urun.Ad = vm.Ad;
+                urun.Fiyat = vm.Fiyat.Value;
+                urun.KategoriId = vm.KategoriId.Value;
+                if (vm.Resim != null)
+                {
+                    ResimSil(urun.ResimYolu);
+                    urun.ResimYolu = ResimYukle(vm.Resim);
+                }
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            KategorileriYukle();
+            return View(vm);
+        }
+
+        private void KategorileriYukle()
+        {
             ViewBag.Kategoriler = _db.Kategoriler
                 .Select(x => new SelectListItem(x.Ad, x.Id.ToString()))
                 .ToList();
-            return View();
+        }
+
+        private void ResimSil(string dosyaAdi)
+        {
+            if (dosyaAdi == null) return;
+            string silmeYolu = Path.Combine(_env.WebRootPath, "img", "urunler", dosyaAdi);
+            try
+            {
+                System.IO.File.Delete(silmeYolu);
+            }
+            catch (Exception) { }
         }
     }
 }
---
+++ BizimMarket/Areas/Admin/Models/UrunDuzenleViewModel.cs
@@ -0,0 +1,29 @@
+﻿using BizimMarket.Attributes;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BizimMarket.Areas.Admin.Models
+{
+    public class UrunDuzenleViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ad alanı zorunlu.")]
+        public string Ad { get; set; }
+
+        [Required(ErrorMessage = "Fiyat alanı zorunlu.")]
+        public decimal? Fiyat { get; set; }
+
+        [Required(ErrorMessage = "Kategori alanı zorunlu.")]
+        public int? KategoriId { get; set; }
+
+        [GecerliResim(MaksimumDosyaBoyutuMb = 2)]
+        public IFormFile Resim { get; set; }
+
+        public string ResimYolu { get; set; }
+    }
+}
